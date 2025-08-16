c07fd057f82c3ae0a92ded7a8478c2163baa7225
Anas MZAATER <anasmzaater@gmail.com>
2025-08-16 23:35:04 +0100

    ajouter les methodes de transfer.

---
+++ ebank-backend/src/main/java/net/anas/ebankbackend/dtos/DebitDTO.java
@@ -0,0 +1,11 @@
+package net.anas.ebankbackend.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class DebitDTO {
+    private String accountId;
+    private double amount;
+    private String description;
+}
--- ebank-backend/src/main/java/net/anas/ebankbackend/web/BankAccountRestController.java
+++ ebank-backend/src/main/java/net/anas/ebankbackend/web/BankAccountRestController.java
@@ -4,19 +4,20 @@ import lombok.AllArgsConstructor;
 import net.anas.ebankbackend.dtos.AccountHistoryDTO;
 import net.anas.ebankbackend.dtos.AccountOperationDTO;
 import net.anas.ebankbackend.dtos.BankAccountDTO;
+import net.anas.ebankbackend.dtos.CreditDTO;
+import net.anas.ebankbackend.dtos.DebitDTO;
+import net.anas.ebankbackend.dtos.TransferRequestDTO;
+import net.anas.ebankbackend.exceptions.BalanceNotSufficientException;
 import net.anas.ebankbackend.exceptions.BankAccountNotFoundException;
 import net.anas.ebankbackend.services.BankAccountService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.security.auth.login.AccountNotFoundException;
 import java.util.List;
 
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin("*")
 public class BankAccountRestController {
     private BankAccountService bankAccountService;
 
@@ -41,5 +42,23 @@ public class BankAccountRestController {
             @RequestParam(name = "page",defaultValue = "0") int page,
             @RequestParam(name = "size",defaultValue = "5") int size) throws BankAccountNotFoundException {
         return bankAccountService.getAccountHistoryByPage(accountId,page,size);
+    }
+
+    @PostMapping("/accounts/debit")
+    public DebitDTO debit(@RequestBody DebitDTO debitDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.debit(debitDTO.getAccountId(),debitDTO.getAmount(),debitDTO.getDescription());
+        return debitDTO;
+    }
+    @PostMapping("/accounts/credit")
+    public CreditDTO credit(@RequestBody CreditDTO creditDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.credit(creditDTO.getAccountId(),creditDTO.getAmount(),creditDTO.getDescription());
+        return creditDTO;
+    }
+    @PostMapping("/accounts/transfer")
+    public void transfer(@RequestBody TransferRequestDTO transferRequestDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.virement(
+                transferRequestDTO.getAccountSource(),
+                transferRequestDTO.getAccountDestination(),
+                transferRequestDTO.getAmount());
     }
 }
